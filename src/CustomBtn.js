ffce1528d0f862f7c564c6be95fff15f257e6f39
Shaykov Alexey <kayfolom@inbox.ru>
2022-06-21 12:45:18 +0300

    aa

--- src/CustomBtn.js
+++ src/CustomBtn.js
@@ -1,7 +1,9 @@
-const CustomBtn = ({ onBtnClick }) => {
+import { memo } from 'react';
+
+const CustomBtn = memo(({ onBtnClick }) => {
   console.log('here is custom btn render ');
 
   return <button onClick={onBtnClick}>Send</button>
-};
+});
 
 export default CustomBtn;
